373361c9010d4b141e71d118254e7a6b344b0aaf
unknown <2fernando2cagliero@gmail.com>
2025-12-02 15:44:56 -0300

    correcion

--- src/componentes/gestion-reclamos/reclamo/asignar-reclamo.tsx
+++ src/componentes/gestion-reclamos/reclamo/asignar-reclamo.tsx
@@ -40,9 +40,6 @@ export default function AsignarReclamoForm({
 
   const methods = useForm<FormValues>({
     resolver: yupResolver(schema) as Resolver<FormValues>,
-    defaultValues: {
-      areaId: reclamo.area.id || "",
-    }
   });
 
   const {
